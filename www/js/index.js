99d1624d3e355fd93211bcdfd2f3e2b8b487a707
tbiscarrues <murilloestacho@hotmail.com>
2016-07-11 13:43:46 +0200

    a

--- www/js/index.js
+++ www/js/index.js
@@ -33,7 +33,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-	window.location="http://aplicaciones.sgv.es/sgvmap/mobile/index.jsp"; 
+	window.location="http://aplicaciones.sgv.es/sgvmap/mobile/index.html"; 
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
